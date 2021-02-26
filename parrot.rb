0a66330d1eedf9f673d9f7166b89c6e61419d28d
Jesus <jose.jesus16@gmail.com>
2021-02-26 00:43:17 -0500

    commit tree dc5cef283a7c010902bbe3a6f308d2e9f456e01e

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+#Create method `parrot` that outputs a given phrase and
+#returns the phrase
+#def parrot (pretty_bird = "Squawk!")
+ #   puts pretty_bird
+#return  "Squawk!"
+#end 
+ #parrot("Pretty bird!")
+
+ def parrot (pretty_bird = "Squawk!")
+    puts  pretty_bird
+    pretty_bird
+end 
